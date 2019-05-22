6b335a448c3efcc2d3a3f0f69e7adaf9ec713dd1
Paulo Ledo <paulo.ledo@elinvar.de>
2019-05-22 02:10:33 +0200

    Adding app.py file

---
+++ app.py
@@ -0,0 +1,11 @@
+
+
+# Data model:
+# -----------
+
+# service_list = []
+# service = {}
+# service["name"] = "devops1" 
+# service["current_image"] = "v1.0.1" 
+# service["registry"] = "arn.blablabla" 
+# service_list.append(service)
